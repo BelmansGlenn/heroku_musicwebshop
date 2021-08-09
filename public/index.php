8aa172321f52dc086af2ddd33077408bd738cf33
BelmansGlenn <belmansglenn@gmail.com>
2021-08-09 14:19:33 +0200

    test5

--- public/index.php
+++ public/index.php
@@ -7,3 +7,11 @@ require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+$url = parse_url(getenv("DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
